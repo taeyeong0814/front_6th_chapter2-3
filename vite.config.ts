a8caae6e629ed4f7c5bb1544b2c09c6b2c81a3e1
이태영 <lhlhgodz3@gmail.com>
2025-08-15 08:24:11 +0900

    :art: 프로젝트 설정 및 의존성 업데이트 및 배포 준비

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,43 @@
-import react from "@vitejs/plugin-react"
 import path from "path"
+
+import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "src"),
-      "@/app": path.resolve(__dirname, "src/app"),
-      "@/pages": path.resolve(__dirname, "src/pages"),
-      "@/widgets": path.resolve(__dirname, "src/widgets"),
-      "@/features": path.resolve(__dirname, "src/features"),
-      "@/entities": path.resolve(__dirname, "src/entities"),
-      "@/shared": path.resolve(__dirname, "src/shared"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://dummyjson.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ command }) => {
+  // 개발 환경인지 확인
+  const isDev = command === "serve"
+
+  return {
+    plugins: [react()],
+    base: "/front_6th_chapter2-3/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"), // 별칭 적용 안했으면 빼도됨
       },
     },
-  },
+    build: {
+      outDir: "dist",
+      sourcemap: false,
+      minify: "esbuild",
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ["react", "react-dom"],
+            query: ["@tanstack/react-query"],
+          },
+        },
+      },
+    },
+    server: {
+      proxy: isDev
+        ? {
+            "/api": {
+              target: "https://dummyjson.com",
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+          }
+        : undefined,
+    },
+  }
 })
